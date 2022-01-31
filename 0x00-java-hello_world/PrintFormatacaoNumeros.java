8edc5aa484fed5f278eae1ddb48daab4b870ae2f
Henrique Nascimento de Carvalho <henriquedc@ciandt.com>
2022-01-31 16:04:07 -0300

    Task 5

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,16 +1,15 @@
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.text.NumberFormat;
 import java.util.Locale;
 
 public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
-        float valor = 7654.322f;
+        float valor = 7654.321f;
         DecimalFormatSymbols symbol = new DecimalFormatSymbols(new Locale("pt","BR"));
         symbol.setDecimalSeparator('.');
         symbol.setGroupingSeparator(',');
-        String pattern = "#,##0.000";
+        String pattern = "#,##0.00";
         DecimalFormat formatador = new DecimalFormat(pattern, symbol);
         System.out.printf("Valor: R$ %s\n", formatador.format(valor));
         System.out.printf("Taxa: %.2f%%", taxa);
