83185d8c226615e3f6bedcdfb4397bf4339a3d71
Henrique Nascimento de Carvalho <henriquedc@ciandt.com>
2022-01-31 15:09:33 -0300

    Task 3

---
+++ 0x00-java-hello_world/PrintStringVariable.java
@@ -0,0 +1,6 @@
+public class PrintStringVariable {
+public static void main(String[] args) {
+    String texto = "Holberton School\n";
+    System.out.println(texto.repeat(2));
+    System.out.println(texto.substring(0,9));
+}}
